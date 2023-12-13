319bd4063ae1fa434b5243fd6f87be7acb775a9d
Gene Kogan <kogan.gene@gmail.com>
2023-12-12 19:32:50 -0800

    abraham bot

--- src/bots/abraham/AbrahamAssistantCog.py
+++ src/bots/abraham/AbrahamAssistantCog.py
@@ -1,31 +1,11 @@
-from pathlib import Path
 from discord.ext import commands
-from cogs.AssistantCog import AssistantCog, LoraInput
-from common.models import EdenAssistantConfig
+from cogs.CharacterCog import CharacterCog
 
 
-class AbrahamAssistantCog(AssistantCog):
+class AbrahamCharacterCog(CharacterCog):
     def __init__(self, bot: commands.bot) -> None:
-        lora = LoraInput(
-            lora_id="6558ee435e91d48ad780de92",
-            lora_strength=0.65,
-            lora_trigger="abraham",
-            require_lora_trigger=True,
-        )
-        assistant_config = EdenAssistantConfig(
-            character_description=self.load_prompt("character_description.txt"),
-            creator_prompt=self.load_prompt("creator_prompt.txt"),
-            documentation_prompt=self.load_prompt("documentation_prompt.txt"),
-            documentation=self.load_prompt("documentation.txt"),
-            router_prompt=self.load_prompt("router_prompt.txt"),
-        )
-        super().__init__(bot, assistant_config, lora)
-
-    def load_prompt(self, fname: str) -> str:
-        path = Path(__file__).parent / "prompts" / fname
-        with open(path, "r") as f:
-            return f.read()
+        super().__init__(bot)
 
 
 def setup(bot: commands.Bot) -> None:
-    bot.add_cog(AbrahamAssistantCog(bot))
+    bot.add_cog(AbrahamCharacterCog(bot))
