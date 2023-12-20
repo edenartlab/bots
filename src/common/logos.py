cf25f74adb29c0cbeab4382d02e5b1c36f9e6dfe
Gene Kogan <kogan.gene@gmail.com>
2023-12-19 20:12:32 -0800

    debug

--- src/common/logos.py
+++ src/common/logos.py
@@ -2,6 +2,12 @@ import requests
 
 
 def request_logos_assistant(api_url: str, assistant: dict, interaction: dict):
+    
+    print("----")
+    print(assistant)
+    print(interaction)
+    print("----")
+    
     response = requests.post(
         f"{api_url}/interact",
         json={
