a7b242563b057a22ce592cfa88b3b98ff43fdf56
Gene Kogan <kogan.gene@gmail.com>
2024-02-01 18:23:59 -0800

    on_welcome

--- src/bots/kojii/EdenLogosCharacterCog.py
+++ src/bots/kojii/EdenLogosCharacterCog.py
@@ -6,6 +6,11 @@ class EdenLogosCharacterCog(LogosCharacterCog):
     def __init__(self, bot: commands.bot) -> None:
         super().__init__(bot)
 
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        print(f"{member} has joined the guild id: {member.guild.id}")
+        await member.send("Welcome to the server! Please read the rules and enjoy your stay!")
+
 
 def setup(bot: commands.Bot) -> None:
     bot.add_cog(EdenLogosCharacterCog(bot))
