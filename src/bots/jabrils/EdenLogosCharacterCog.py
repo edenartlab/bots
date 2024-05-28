adf719774192b985266f1c81db09ed7581f20870
genekogan <kogan.gene@gmail.com>
2024-05-27 17:02:20 -0700

    put back

--- src/bots/jabrils/EdenLogosCharacterCog.py
+++ src/bots/jabrils/EdenLogosCharacterCog.py
@@ -1,11 +1,11 @@
-# from discord.ext import commands
-# from cogs.Eden2Cog import Eden2Cog
+from discord.ext import commands
+from cogs.Eden2Cog import Eden2Cog
 
 
-# class EdenEden2Cog(Eden2Cog):
-#     def __init__(self, bot: commands.bot) -> None:
-#         super().__init__(bot)
+class EdenEden2Cog(Eden2Cog):
+    def __init__(self, bot: commands.bot) -> None:
+        super().__init__(bot)
 
 
-# def setup(bot: commands.Bot) -> None:
-#     bot.add_cog(EdenEden2Cog(bot))
+def setup(bot: commands.Bot) -> None:
+    bot.add_cog(EdenEden2Cog(bot))
